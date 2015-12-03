fbc6fc0bdac157b2c47215cedc9dd10a38618e16
Stephen Stadler <stephen.stadler@nike.com>
2015-12-03 14:56:31 -0800

    Adding NikeFuel model and updating page/script

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# jsug
-
 JavaScript User Group
 =====================
 
@@ -8,20 +6,32 @@ The files used for my WebGL presentation
 Technologies
 ------------
 
-- three.js
-- J3D
-- Unity
+- [three.js](https://github.com/mrdoob/three.js)
+- [Unity](http://unity3d.com/)
+- [Blender](https://www.blender.org/)
+- [Cardboard SDK for Unity](https://developers.google.com/cardboard/unity/)
 
 ## How-To Use
 1. Clone this repo
 
 Build/Run three.js project
+
 1. `npm install` in three.js dir
+2. Put three folder onto a webserver
 
 Build/Run Unity project
-1. Install [Unity](http://unity3d.com/)
+
+1. Install Unity
 2. Open Unity project
+3. Press play
 
 ## Demo
 
-Nike+ Endless Runner ("Pre" Runner)
+Nike+ "Pre" Runner
+
+Resources
+------------
+
+- [Getting Started with Unity for Android](https://developers.google.com/cardboard/unity/get-started)
+- [Your Own Subway Surfer/Temple Run Like Game](http://www.thegamecontriver.com/p/your-own-subway.html)
+- [J3D](https://github.com/drojdjou/J3D)
--- three/js/nike+.js
+++ three/js/nike+.js
@@ -1,29 +1,60 @@
-window.onload = function(){
-    var 
-        shapeObjectUrl = "models/nikeplus.json",
-        scene = new THREE.Scene(),
-        camera = new     THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1, 1000),
-        renderer = new THREE.WebGLRenderer(),
-        loader = new THREE.JSONLoader();
+var scene, camera, renderer;
+var geometry, material, mesh;
+var loader;
+var dirLight, pointLight;
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+init();
+//animate();
 
-    loader.load( shapeObjectUrl, 
-        function ( geometry, materials ) {
-            console.log(geometry, materials);
-            var mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial() );
-            mesh.rotation.x = 90;
-            scene.add( mesh );
-            console.log(mesh);
-            render();
-    });
+function init() {
 
+    scene = new THREE.Scene();
+
+    // LIGHTS
+    dirLight = new THREE.DirectionalLight( 0xffffff, 0.125 );
+    dirLight.position.set( 0, 0, 1 ).normalize();
+    scene.add( dirLight );
+
+    pointLight = new THREE.PointLight( 0xffffff, 1.5 );
+    pointLight.position.set( 0, 100, 90 );
+    scene.add( pointLight );
+
+    // CAMERA
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 10000 );
     camera.position.z = 5;
 
-    var render = function () {
-        requestAnimationFrame(render);
-        renderer.render(scene, camera);
-    };
+    //geometry = new THREE.BoxGeometry( 200, 200, 200 );
+    material = new THREE.MeshLambertMaterial( { color: 0xfa5400 } );
 
-};
+    //mesh = new THREE.Mesh( geometry, material );
+    //scene.add( mesh );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    loader = new THREE.JSONLoader();
+    loader.load("models/nikeplus.json", onLoad);
+
+    document.body.appendChild( renderer.domElement );
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    mesh.rotation.z += 0.01;
+
+    renderer.render( scene, camera );
+
+}
+
+function onLoad(geometry, materials) {
+
+    mesh = new THREE.Mesh( geometry, material );
+    mesh.rotation.x = 90;
+    scene.add( mesh );
+
+    animate();
+
+}
