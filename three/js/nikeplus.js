aa0d4d7a57c3fb478ae112bd33fa35b3689eb457
Stephen Stadler <stephen.stadler@nike.com>
2015-12-15 11:57:33 -0800

    Cleaning up

--- three/js/nikeplus.js
+++ three/js/nikeplus.js
@@ -4,7 +4,6 @@ var loader;
 var dirLight, pointLight;
 
 init();
-//animate();
 
 function init() {
 
@@ -23,15 +22,12 @@ function init() {
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 10000 );
     camera.position.z = 5;
 
-    //geometry = new THREE.BoxGeometry( 200, 200, 200 );
     material = new THREE.MeshLambertMaterial( { color: 0xfa5400 } );
-
-    //mesh = new THREE.Mesh( geometry, material );
-    //scene.add( mesh );
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
+    // ACTION
     loader = new THREE.JSONLoader();
     loader.load("models/nikeplus.json", onLoad);
 
